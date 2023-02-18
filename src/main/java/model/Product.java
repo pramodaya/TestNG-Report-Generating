62fd46bf949a7a08a49b58e644aa581e817fd6d4
pramodaya <pramodayajayalath@gmail.com>
2023-02-19 00:32:25 +0200

    Test cases add

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,2 +1,106 @@
-package model;public class Product {
+package model;
+
+public class Product {
+    private float id;
+    private String name;
+    private String description;
+    private String manufacturer;
+    private String category;
+    private float price;
+    private String created;
+    private String status;
+    private String tags;
+
+
+    // Getter Methods
+
+    public float getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    // Setter Methods
+
+    public void setId(float id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", created='" + created + '\'' +
+                ", status='" + status + '\'' +
+                ", tags='" + tags + '\'' +
+                '}';
+    }
 }
+
--- src/test/java/API_Test/Test_Product.java
+++ src/test/java/API_Test/Test_Product.java
@@ -1,2 +1,51 @@
-package API_Test;public class Test_Product {
+package API_Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import model.Product;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test_Product {
+
+    @Test(priority = 1)
+    public void test_getAllProductCategories(){
+        given()
+                .when()
+                .get("https://mystoreapi.com/catalog/categories")
+                .then()
+                .statusCode(200)
+                .log()
+                .body();
+    }
+
+    @Test(priority = 2)
+    public void test_getAllDataInProductCategory(){
+        given()
+                .when()
+                .get("https://mystoreapi.com/catalog/products")
+                .then()
+                .statusCode(200)
+                .log()
+                .body();
+    }
+    @Test(priority = 3)
+    public void test_getProductById(){
+
+
+        Product resp =
+        given()
+                .when()
+                .get("https://mystoreapi.com/catalog/product/20774")
+                .then()
+                .statusCode(200)
+                .log()
+                .body()
+                .extract().body().as(Product.class);
+        Assert.assertEquals(resp.getName(), "tree");
+    }
 }
